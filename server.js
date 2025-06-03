5793a177b09d8c2c050ef313645c6fcbfafa6ce2
NK <snitinkuma987@gmail.com>
2025-06-03 23:34:24 +0530

    Remove frontend serving on backend

--- server.js
+++ server.js
@@ -29,11 +29,11 @@ app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.get("*", (req, res) => {
-	res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-});
+// app.get("*", (req, res) => {
+// 	res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+// });
 
 
 app.listen(PORT, () => {
